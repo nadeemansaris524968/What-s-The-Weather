4342b09b91bd574180d8b283a08a55e0de6817e9
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-05-02 12:30:37 -0500

    App is now complete. Implemented multithreading to facilitate displaying output when the output is ready.

--- What's The Weather/ViewController.swift
+++ What's The Weather/ViewController.swift
@@ -18,8 +18,55 @@ class ViewController: UIViewController {
     
     @IBAction func submitBTN(_ sender: Any) {
         
-        
-        
+        if let url = URL(string: "http://www.weather-forecast.com/locations/"+cityTF.text!.replacingOccurrences(of: " ", with: "-")+"/forecasts/latest") {
+            let request = NSMutableURLRequest(url: url)
+            var message = ""
+            let task = URLSession.shared.dataTask(with: request as URLRequest) { (Data, Response, Error) in
+                
+                if Error != nil {
+                    print(Error!)
+                }
+                else {
+                    if Data != nil {
+                        let unwrappedData = Data!
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        let stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                        
+                        if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                                                       
+                            if (contentArray.count) > 1 {
+                                
+                                let potentialOutput = contentArray[1]
+                                
+                                let finalOutput = potentialOutput.components(separatedBy: "</span>")
+                                
+                                if finalOutput.count > 1 {
+                                    message = finalOutput[0].replacingOccurrences(of: "&deg;", with: "\u{00B0}")
+                                    
+                                    print(message)
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                if message == "" {
+                    message = "The weather couldn't be found. Try again later."
+                }
+                DispatchQueue.main.sync(execute: {
+                    
+                    self.outputLBL.isHidden = false
+                    self.outputLBL.text = message
+                    
+                })
+            }
+            task.resume()
+        }
+        else {
+            outputLBL.text = "Error"
+        }
+    
     }
 
     override func viewDidLoad() {
@@ -29,42 +76,7 @@ class ViewController: UIViewController {
         submitBTN.layer.cornerRadius = 5
         outputLBL.layer.masksToBounds = true
         outputLBL.layer.cornerRadius = 5
-//        outputLBL.isHidden = true
-        
-        let url = URL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
-        let request = NSMutableURLRequest(url: url!)
-        let task = URLSession.shared.dataTask(with: request as URLRequest) { (Data, Response, Error) in
-            
-            if Error != nil {
-                print(Error!)
-            }
-            else {
-                if Data != nil {
-                    let unwrappedData = Data!
-                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                    
-                    let stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
-                    
-                    if (dataString?.components(separatedBy: stringSeparator)) != nil {
-                        
-                        let contentArray = dataString?.components(separatedBy: stringSeparator)
-                        
-                        if (contentArray?.count)! > 0 {
-                            
-                            let potentialOutput = contentArray![1]
-                            
-                            let finalOutput = potentialOutput.components(separatedBy: "</span>")[0]
-                            
-                            print(finalOutput)
-                        }
-                        
-                    }
-                }
-            }
-            
-        }
-        task.resume()
-        
+        outputLBL.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
