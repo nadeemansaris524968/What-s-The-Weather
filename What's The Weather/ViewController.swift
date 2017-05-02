a9ce32f91e93625692fc9db1996b84e9e8ceda9e
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-05-02 09:54:54 -0500

    Init

--- What's The Weather/ViewController.swift
+++ What's The Weather/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var submitBTN: UIButton!
+    
+    @IBOutlet weak var outputLBL: UILabel!
+    
+    @IBOutlet weak var cityTF: UITextField!
+    
+    @IBAction func submitBTN(_ sender: Any) {
+        
+        
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        submitBTN.layer.cornerRadius = 5
+        outputLBL.layer.masksToBounds = true
+        outputLBL.layer.cornerRadius = 5
+        outputLBL.isHidden = true
+        
+        let url = URL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
+        let request = NSMutableURLRequest(url: url!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { (Data, Response, Error) in
+            
+            if Error != nil {
+                print(Error!)
+            }
+            else {
+                if Data != nil {
+                    let unwrappedData = Data!
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    print(dataString!)
+                }
+            }
+            
+        }
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
